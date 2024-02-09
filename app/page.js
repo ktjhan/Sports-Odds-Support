7097bea41fd015423ffeecb5488395acaf531253
Kavin Tjhan <tjhan.kavin@gmail.com>
2024-02-08 20:14:18 -0800

    first commit for support page

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Support from "./support";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-evenly p-24">
+      <Support />
+    </main>
+  );
+}
---
+++ app/support.js
@@ -0,0 +1,23 @@
+const Support = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-3xl font-bold">Welcome to Sports Betting Odds!</h1>
+      <p>
+        If you&apos;re in need of support, please feel free to ping us an email
+        🙂
+      </p>
+      <a
+        href="mailto:ktjhan@protonmail.com"
+        style={{
+          marginTop: "25px",
+          color: "blue",
+          textDecoration: "underline",
+        }}
+      >
+        e-mail us!
+      </a>
+    </div>
+  );
+};
+
+export default Support;
